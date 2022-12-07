0879af1c55998c7f41aeb2d446cd8c93a58c49d5
JustoRago <asdt560@gmail.com>
2022-12-07 12:34:55 -0300

    Add new tables

--- schema.sql
+++ schema.sql
@@ -8,6 +8,21 @@ CREATE TABLE animals(
     escape_attempts INT, 
     neutered BOOLEAN, 
     weight_kg decimal,
-    species VARCHAR(255),
+    species_id VARCHAR(255) references species(name),
+    owner_id VARCHAR(255) references owner(name),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE owners(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(255),
+    age INT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE species(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
