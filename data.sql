494b98b4349031ef6b3b44750caa41c1b94f6f40
JustoRago <asdt560@gmail.com>
2022-12-07 13:40:44 -0300

    Fix problem

--- data.sql
+++ data.sql
@@ -17,7 +17,6 @@ INSERT INTO owners(full_name, age) VALUES('Jennifer Orwell', 19);
 INSERT INTO owners(full_name, age) VALUES('Bob', 45);
 INSERT INTO owners(full_name, age) VALUES('Melody Pond',  77);
 INSERT INTO owners(full_name, age) VALUES('Dean Winchester', 14);
-INSERT INTO owners(full_name, age) VALUES('Jodie Whittaker', 38);
 
 INSERT INTO species(name) VALUES('Pokemon');
 INSERT INTO species(name) VALUES('Digimon');
